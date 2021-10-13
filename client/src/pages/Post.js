e6ae97a12bcaab2216a3ad124b81ca4b7827eb74
JLopezz3 <lopezjerel@gmail.com>
2021-10-13 11:29:42 -0500

    addComments function

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -6,13 +6,30 @@ function Post() {
 
     let { id } = useParams();
     const [postObject, setPostObject] = useState({});
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState("");
 
     useEffect(() => {
         axios.get(`http://localhost:5000/posts/byId/${id}`).then((response) => {
             setPostObject(response.data);
-            console.log(response.data);
         }); 
-    });
+
+        axios.get(`http://localhost:5000/comments/${id}`).then((response) => {
+            setComments(response.data);
+        }); 
+    }, []);
+
+    const addComment = () => {
+        axios.post("http://localhost:5000/comments", { 
+            commentBody: newComment, 
+            PostId: id 
+        })
+        .then((reponse) => {
+            const commentToAdd = {commentBody: newComment}
+            setComments([...comments, commentToAdd]);
+            setNewComment("");
+        })
+    }
 
     return (
         <div className="postPage">
@@ -23,7 +40,26 @@ function Post() {
                     <div className="footer">{postObject.username}</div>
                 </div>
             </div> 
-            <div className="rightSide">Comment Section</div>
+            <div className="rightSide">
+                <div className="addCommentContainer">
+                    <input 
+                        type="text" 
+                        placeholder="Comment..." 
+                        autoComplete="off" 
+                        value={newComment}
+                        onChange={(event) => {setNewComment(event.target.value)}} />
+                    <button onClick={addComment}> Add Comment </button>
+                </div>
+                <div className="listOfComments">
+                    {comments.map((comment, key) => {
+                        return ( 
+                            <div key={key} className="comment"> 
+                                {comment.commentBody} 
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
